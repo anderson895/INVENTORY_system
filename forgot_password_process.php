90197a947bd735cd8952eaa49d34cf54737bcca9
JARP <105678913+anderson895@users.noreply.github.com>
2025-10-04 22:11:14 +0800

    update

--- forgot_password_process.php
+++ forgot_password_process.php
@@ -32,8 +32,8 @@ if(isset($_POST['email'])){
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'depedtrack264@gmail.com';
-        $mail->Password   = 'krib ldvp jhfw orvn';
+        $mail->Username   = 'johnzausapogi@gmail.com';
+        $mail->Password   = 'chsm uhit eyyn yhmj';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
